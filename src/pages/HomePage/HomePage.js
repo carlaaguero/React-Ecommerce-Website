0aba9cce4b3671522a61f1c8e0d44ef9bb442466
carlaaguero <c.agueromiranda@gmail.com>
2024-02-04 18:42:08 +0100

    Configurating product display in different categories, implementing slides, setting up product titles and prices, and adding responsive CSS design.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,16 +3,22 @@ import Slider from '../../components/Slider/Slider';
 import "./HomePage.scss";
 import Category from '../../components/Category/Category';
 import ProductList from '../../components/ProductList/ProductList';
+import SingleCategory from '../../components/SingleCategory/SingleCategory';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCategories, fetchProductsByCategory } from '../../store/categorySlice';
+import { fetchProducts } from '../../store/productSlice';
+
 
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const {data: categories, status: categoryStatus} = useSelector((state) => state.category);
   const {catProductAll: productsByCategory, catProductAllStatus} = useSelector((state) => state.category);
+  const {data: products, status: productStatus} = useSelector((state) => state.product);
+
 
   useEffect(() => {
+    dispatch(fetchProducts());
     dispatch(fetchCategories());
     dispatch(fetchProductsByCategory(1, 'all'));
     dispatch(fetchProductsByCategory(2, 'all'));
@@ -22,6 +28,13 @@ const HomePage = () => {
     <div className='home-page'>
       <Slider />
       <Category categories = {categories} status = {categoryStatus} />
+      <ProductList products = {products} status = {productStatus} />
+      <section>
+        { productsByCategory[0] && <SingleCategory products = {productsByCategory[0]} status = {catProductAllStatus} /> }
+      </section>
+      <section>
+        { productsByCategory[1] && <SingleCategory products = {productsByCategory[1]} status = {catProductAllStatus} /> }
+      </section>
     </div>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoryReducer from "./categorySlice";
+import modalReducer from "./modalSlice";
+import productReducer from "./productSlice";
+
+
 
 const store = configureStore({
     reducer: {
+        product: productReducer,
         category: categoryReducer,
+        modal: modalReducer,
+
+        
     }
 });
 
