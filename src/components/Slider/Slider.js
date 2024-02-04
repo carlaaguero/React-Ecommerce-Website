c527f18a841577643b09562c5da43d282255e6a8
carlaaguero <c.agueromiranda@gmail.com>
2024-02-04 12:19:18 +0100

    Creating HomePage section, configuring Category Products section, adding Error and Loader, and implementing CSS with responsive design

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./Slider.scss";
-
+import {sliderImages} from '../../utils/images';
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className = "hero-slider">
+      <div className='hero-slider-item'>
+        <img src = {sliderImages[1]} alt = "" />
+      </div>
+    </div>
   )
 }
 
